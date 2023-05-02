c1449b9f8fd0c49d37080d8f76dd25721f8396c0
sk-dev-hub <yolloboom@gmail.com>
2023-05-02 17:19:26 +0300

    start setup

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="MainLayout" class="w-full fixed z-50">
+        <div
+            id="TopMenu"
+            class="w-full bg-[#FAFAFA] border-b md:block hidden"
+        >
+            <ul
+                class="
+                    flex
+                    items-center
+                    justify-end
+                    text-xs
+                    text-[#333333]
+                    font-light
+                    px-2
+                    h-10
+                    bg-[#FAFAFA]
+                    max-w-[1200px]
+                "
+            >
+
+            </ul>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    pages: true,
+    modules: [
+        'nuxt-icon',
+        'nuxt-lodash',
+        '@pinia/nuxt',
+        '@pinia-plugin-persistedstate/nuxt',
+        '@nuxtjs/tailwindcss',
+        //'@nuxtjs/supabase',
+    ],
+
+})
