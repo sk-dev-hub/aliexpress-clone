c03cf0764c9418da42e49da7aa84b2659e7bda4d
sk-dev-hub <yolloboom@gmail.com>
2023-05-03 18:03:38 +0300

    ProductComponent

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
     <MainLayout>
         <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-            IndexPage
+            <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+                <div v-if="products" class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">    
+                        <div  v-for="product in products" :key="product">
+                            <ProductComponent :product="product"/>
+                        </div>            
+                </div>
+            </div>
         </div>
     </MainLayout>
 </template>
@@ -9,5 +15,46 @@
 <script setup>
 import MainLayout from "~/layouts/MainLayout.vue"
 
+const products = [
+    {
+        id: 1,
+        title: "Title 1",
+        description: "This is description",
+        url: "https://loremflickr.com/800/800",
+        price: 9344
 
+    },
+    {
+        id: 2,
+        title: "Title 2",
+        description: "This is description",
+        url: "https://loremflickr.com/800/800",
+        price: 9422
+
+    },
+    {
+        id: 3,
+        title: "Title 3",
+        description: "This is description",
+        url: "https://loremflickr.com/800/800",
+        price: 9655
+
+    },
+    {
+        id: 4,
+        title: "Title 4",
+        description: "This is description",
+        url: "https://loremflickr.com/800/800",
+        price: 9999
+
+    },
+    {
+        id: 5,
+        title: "Title 5",
+        description: "This is description",
+        url: "https://loremflickr.com/800/800",
+        price: 3459
+
+    }
+]
 </script>
